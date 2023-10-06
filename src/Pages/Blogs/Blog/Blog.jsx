6ea12046771336fc892771163b8f839b113e0924
Ashfakul Islam <r4pido3@gmail.com>
2023-10-07 00:13:48 +0600

    blogs page added

---
+++ src/Pages/Blogs/Blog/Blog.jsx
@@ -0,0 +1,26 @@
+const Blog = ({ blog }) => {
+  const {title, author, date, image_url, excerpt} = blog;
+  return (
+    <div>
+      <div className="flex rounded-xl flex-col items-center lg:flex-row bg-base-100 shadow-xl">
+        <div>
+          <img
+          className="rounded-t-xl lg:rounded-l-xl lg:rounded-r-none "
+            src={image_url}
+            alt={title}
+          />
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{date}</p>
+          <p>{excerpt}</p>
+          <div className="card-actions mt-5 justify-center">
+            <button className="btn bg-black text-white hover:text-black">Read More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Blog from "./Blog/Blog";
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(()=> {
+        fetch('Blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+    return (
+        <div className="my-16 mx-5">
+            <h1 className="text-center mb-10 text-3xl">Blogs</h1>
+        <div className="max-w-5xl mx-auto flex flex-col gap-5 ">
+            {
+                blogs.map(blog => {
+                    return(
+                        <Blog key={blog.id} blog={blog}/>
+                    )
+                })
+            }
+        </div>
+        </div>
+    );
+};
+
+export default Blogs;
